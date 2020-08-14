8cb0fdaa6badd0d1d828371d93d9ba69a324b622
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-13 13:28:22 -1200

    Update update-customer.php

--- dashboard/update-customer.php
+++ dashboard/update-customer.php
@@ -14,7 +14,20 @@ if(isset($_POST['updatecustomer']))
    $use6 = $_POST['status'];
 
    
-   $query = "UPDATE customer SET name='$use1', number='$use2', date='$use3', month='$use4',amount='$use5',status='$use6'
+  if(isset($_POST['updatecustomer']))
+{
+   $id = $_POST['edit_id'];
+   $use1 = $_POST['name'];
+   $use2 = $_POST['number'];
+   $use3 = $_POST['date'];
+   $use4 = $_POST['month'];
+   $use5 = $_POST['amount'];
+   $use6 = $_POST['status'];
+   $use7 = $_POST['property'];
+   $use8 = $_POST['balance'];
+
+   
+   $query = "UPDATE customer SET name='$use1', number='$use2', date='$use3', month='$use4',amount='$use5',status='$use6',property='$use7',balance='$use8'
     WHERE id='$id'";
    $query_run = mysqli_query($connection, $query);
 
