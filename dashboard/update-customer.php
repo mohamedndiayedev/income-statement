94fce7e07d216500f9938805cb29051b4d9a8149
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-13 13:40:16 -1200

    Update update-customer.php

--- dashboard/update-customer.php
+++ dashboard/update-customer.php
@@ -1,18 +1,6 @@
 <?php
 session_start();
  $connection = mysqli_connect("mkorvuw3sl6cu9ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","iigi5330azzahj3c","smij1gf9l7l65kwi","jj0m1ku5w7a37l39");
-
-
-if(isset($_POST['updatecustomer']))
-{
-   $id = $_POST['edit_id'];
-   $use1 = $_POST['name'];
-   $use2 = $_POST['number'];
-   $use3 = $_POST['date'];
-   $use4 = $_POST['month'];
-   $use5 = $_POST['amount'];
-   $use6 = $_POST['status'];
-
    
   if(isset($_POST['updatecustomer']))
 {
